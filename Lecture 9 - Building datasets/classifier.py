ad9de0be579d89a6ca288e002b0dad0d98b7de2f
HB <harryaberg@gmail.com>
2019-02-11 20:36:25 +0000

    latest classifier

--- Lecture 9 - Building datasets/classifier.py
+++ Lecture 9 - Building datasets/classifier.py
@@ -33,9 +33,10 @@ k
 
 epochs = 10
 batch_size = 2
-
 units1 = 128
 units2 = 32
+dropout_p = 0.5
+m = len(dataset)
 
 
 train_size = int(0.8 * len(dataset))
@@ -46,6 +47,9 @@ train_loader = DataLoader(dataset=train_dataset,
                           batch_size=batch_size,
                           shuffle=True)
 
+val_loader = DataLoader(dataset=val_dataset,
+                        batch_size=m)
+
 class NN(torch.nn.Module):
 
     def __init__(self):
@@ -54,8 +58,10 @@ class NN(torch.nn.Module):
         self.regressor = torch.nn.Sequential(
             torch.nn.Linear(512 * 7 * 7, units1),
             torch.nn.Sigmoid(),
+            torch.nn.Dropout(dropout_p),
             torch.nn.Linear(units1, units2),
             torch.nn.Sigmoid(),
+            torch.nn.Dropout(dropout_p),
             torch.nn.Linear(units2, 1),
             torch.nn.Sigmoid()
         )
@@ -66,36 +72,65 @@ class NN(torch.nn.Module):
         return x
 
 nn = NN()
-criterion = torch.nn.BCELoss()
-optimiser = torch.optim.SGD(params=nn.parameters(), lr=0.5, momentum=0.1)
+criterion = torch.nn.BCELoss(reduction='mean')
+optimiser = torch.optim.SGD(params=nn.parameters(), lr=0.7, momentum=0.1)
 
 
-def train():
+def train(model, fig, ax, idx):
+    for batch_idx, batch in enumerate(train_loader):
+        x, y = batch
+        #print(batch)
+        h = model(x)
+        #print('Prediction:', h.item(), 'Label:', y.item())     # for batch size == 1
 
+        loss = criterion(h, y.float())
+        optimiser.zero_grad()
+        loss.backward()
+        optimiser.step()
+
+        #print('Epoch:', epoch, '\tBatch:', batch_idx, '\tLoss:', loss.item())
+        #print()
+        #batch_losses.append(loss.item())
+        ax.scatter(idx, loss.item(), c='b', s=5)
+        #losses.append(np.mean(batch_losses))
+        fig.canvas.draw()
+        idx += 1
+    return idx
+
+
+def test(model, fig, ax, idx):
+    for batch_idx, batch in enumerate(val_loader):
+        x, y = batch
+        h = model(x)
+        loss = criterion(h, y.float())
+        ax.scatter(idx, loss.item(), c='r')
+        fig.canvas.draw()
+    pass
+
+def execute(model, epochs=epochs):
+
+    # SET UP PLOTTING
     fig = plt.figure()
     ax = fig.add_subplot(111)
     plt.grid()
     plt.ion()
     plt.show()
-    losses = []
-    for epoch in range(epochs):
-        batch_losses = []
-        for batch_idx, batch in enumerate(train_loader):
-            x, y = batch
-            #print(batch)
-            h = nn(x)
-            #print('Prediction:', h.item(), 'Label:', y.item())     # for batch size == 1
 
-            loss = criterion(h, y.float())
-            optimiser.zero_grad()
-            loss.backward()
-            optimiser.step()
-            print('Epoch:', epoch, '\tBatch:', batch_idx, '\tLoss:', loss.item())
-            print()
-            batch_losses.append(loss.item())
-            ax.plot(losses, c='b')
-            fig.canvas.draw()
-        losses.append(np.mean(batch_losses))
+    losses = []
+    batch_losses = []
+
+    idx = 0
+
+    for epoch in range(epochs):
+        train(model, fig, ax, idx)
+        losses.append()
+        print(idx)
+        test(model, fig, ax, idx)
+
+
     plt.ioff()
 
-train()
+execute(nn)
+
+import time
+torch.save(nn.state_dict(), str(time.time()))
